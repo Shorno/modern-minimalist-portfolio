1ceead98136ae5f7e2d1a99f205eca6dcee35498
Shorno <fb.shrono@gmail.com>
2024-05-29 15:44:22 +0600

    hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,7 @@
 import {Spotlight} from "@/components/ui/Spotlight";
+import {TextGenerateEffect} from "@/components/ui/text-generate-effect";
+import MagicButton from "@/components/ui/MagicButton";
+import {FaLocationArrow} from "react-icons/fa6";
 
 
 const Hero = () => {
@@ -11,13 +14,34 @@ const Hero = () => {
             </div>
 
             <div
-                className="h-[50rem] w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
-                {/* Radial gradient for the container to give a faded look */}
+                className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
+       absolute top-0 left-0 flex items-center justify-center"
+            >
                 <div
-                    className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+                    className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
+         bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
+                />
             </div>
-            <div>
 
+
+            <div className={"flex justify-center relative my-10 z-10"}>
+                <div className={"max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center"}>
+                    <h2 className={"uppercase tracking-widest text-xs text-center text-blue-100 max-w-80"}>
+                        Dynamic Web Magic With Next.Js
+                    </h2>
+                    <TextGenerateEffect words={"Transforming Concepts Into Seamless User Experience"} className={"text-center text-[40px] md:text-5xl lg:text-6xl"}/>
+
+                    <p className={"text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl"}>
+                        Hi, I&apos;m Shorno, a Next.js developer based in Dhaka.
+                    </p>
+                    <a href="#about">
+                        <MagicButton
+                        title={"Show Projects"}
+                        icon={<FaLocationArrow/>}
+                        position={"right"}
+                        />
+                    </a>
+                </div>
             </div>
 
         </div>
